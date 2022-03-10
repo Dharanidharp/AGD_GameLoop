7da92df4feb94d2068a308f48ee2359c2296979b
quarkzz <pdharani18.dp@gmail.com>
2022-03-11 00:36:06 +0530

    SFML

---
+++ GameLoopTests/test.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <SFML/Graphics.hpp>
+
+
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(Window, Init)
+{
+	//Window w;
+	sf::RenderWindow wr;
+}
